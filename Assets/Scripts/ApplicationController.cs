9aca3f97cd7b23ebaf7e05c866080d3fec4d8c06
aeciobrito@gmail.com <aeciobrito@gmail.com>
2019-04-08 12:37:18 -0300

    Server script and vuforia implemented. Ui Changes for access different scenes.

--- Assets/Scripts/ApplicationController.cs
+++ Assets/Scripts/ApplicationController.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using TMPro;
 using System;
 
 public class ApplicationController : MonoBehaviour
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject _mainMenu, _clientMenu, _serverMenu;
+    private GameObject _currentMenu;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _mainMenu.SetActive(true);
+        _clientMenu.SetActive(false);
+        _clientMenu.SetActive(false);
+
+        _currentMenu = _mainMenu;
+	}
+	
+    public void ActiveMenu(GameObject menuToActive)
+    {
+        _currentMenu.SetActive(false);
+        menuToActive.SetActive(true);
+        _currentMenu = menuToActive;
+    }
+
+}
